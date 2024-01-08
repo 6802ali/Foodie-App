31482f566a8e151d07044fe6ae4faa2cfff97496
6802ali <ali2006367@miuegypt.edu.eg>
2024-01-08 04:55:48 +0200

    adding the reservation

--- lib/Screens/Restaurantdetailswidgets/restaurantsdetails.dart
+++ lib/Screens/Restaurantdetailswidgets/restaurantsdetails.dart
@@ -1,6 +1,8 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:foodie/Firestore/Models/Meal.dart';
+import 'package:foodie/Firestore/Models/Reservation.dart';
 import 'package:foodie/Firestore/Models/Restaurant.dart';
 import 'package:foodie/Firestore/Services/MealService.dart';
 import 'package:foodie/Models/MenuModel.dart';
@@ -18,7 +20,10 @@ class RestaurantDetailsPage extends StatefulWidget {
 }
 
 class _RestaurantDetailsPageState extends State<RestaurantDetailsPage> {
+  DateTime dateTime = DateTime(2024, 02, 25, 12, 30);
+
   List<Meal> meals = [];
+  List<Reservation> reservation = [];
   bool showMenu = false;
 
   @override
@@ -50,18 +55,6 @@ class _RestaurantDetailsPageState extends State<RestaurantDetailsPage> {
 
     return image;
   }
-
-  /* Future<void> getData() async {
-    try {
-      List<MenuModel> fetchedMenu = await FireRestaurant.getRestaurantMenu(widget.restaurant.id);
-      setState(() {
-        menu = fetchedMenu;
-        showMenu = true; // Set showMenu to true after fetching data
-      });
-    } catch (e) {
-      print('Error fetching menu: $e');
-    }
-  } */
 
   @override
   Widget build(BuildContext context) {
@@ -116,8 +109,57 @@ class _RestaurantDetailsPageState extends State<RestaurantDetailsPage> {
                         child: Text("Menu"),
                       ),
                       ElevatedButton(
-                        onPressed: () {
-                          // Add your onPressed logic for "Reservations" button
+                        onPressed: () async {
+                          DateTime? newDate = await showDatePicker(
+                            context: context,
+                            initialDate: dateTime,
+                            firstDate: DateTime.now(),
+                            lastDate: DateTime(2025),
+                          );
+                          if (newDate == null) return;
+
+                          TimeOfDay? newTime = await showTimePicker(
+                              context: context,
+                              initialTime: TimeOfDay(
+                                  hour: dateTime.hour,
+                                  minute: dateTime.minute));
+
+                          if (newTime == null) return;
+
+                          final newDateTime = DateTime(
+                            newDate.year,
+                            newDate.month,
+                            newDate.day,
+                            newTime.hour,
+                            newTime.minute,
+                          );
+
+                          setState(() {
+                            dateTime = newDateTime;
+                          });
+                          print('datetimetest----------------------------');
+                          print(dateTime);
+                          print(DateTime.now());
+                          print(dateTime.hour);
+                          if (dateTime.hour < dateTime.minute) {
+                            AwesomeDialog(
+                              context: context,
+                              dialogType: DialogType.error,
+                              animType: AnimType.rightSlide,
+                              title: 'Error',
+                              desc: 'Reservation already booked',
+                              btnOkOnPress: () {},
+                            ).show();
+                          }else{
+                            AwesomeDialog(
+                              context: context,
+                              dialogType: DialogType.success,
+                              animType: AnimType.rightSlide,
+                              title: 'success',
+                              desc: 'reservation completed',
+                              btnOkOnPress: () {},
+                            ).show();
+                          }
                         },
                         child: Text("Reservations"),
                       ),
@@ -181,183 +223,3 @@ class _RestaurantDetailsPageState extends State<RestaurantDetailsPage> {
             }));
   }
 }
-
-
-
-//   Widget buildMealList(List<MenuModel> menuList) {
-//     return ListView.builder(
-//       itemCount: menuList.length, //cheeseburger, rebeye, chickenfile, .length 3
-//       itemBuilder: (context, index) {
-//         MenuModel menu = menuList[index]; //data of cheese
-
-//         return Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             SizedBox(height: 8),
-//             ListView.builder(
-//               itemCount: menu.meals.length,
-//               itemBuilder: (context, mealIndex) {
-//                 MealModel meal = menu.meals[mealIndex];
-//                 return ListTile(
-//                   contentPadding: EdgeInsets.all(8.0),
-//                   title: Text(
-//                     meal.mealName,
-//                     style: TextStyle(
-//                         color: Colors.black, fontWeight: FontWeight.bold),
-//                   ),
-//                   subtitle: Text(
-//                     meal.mealDescription,
-//                     style: TextStyle(color: Colors.black),
-//                   ),
-//                 );
-//               },
-//             ),
-//             Divider(), // Add a divider between menu categories
-//           ],
-//         );
-//       },
-//     );
-//   }
-// }
-
-// Widget BuildMealCard(List<MenuModel> menuList) {
-//   return ListView.builder(
-//       itemCount: menuList.length,
-//       itemBuilder: (context, index) {
-//         MenuModel menu = menuList[index];
-//         Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-//           SizedBox(height: 8),
-//           ListView.builder(
-//               itemCount: menu.meals.length,
-//               itemBuilder: (context, mealIndex) {
-//                 MealModel meal = menu.meals[mealIndex];
-//                 return Card();
-//               })
-//         ]);
-
-        // return Card(
-        //     child:ListView.builder(  menu.meals.length,itemBuilder: (context, mealIndex)){
-        //       MealModel meal = menu.meals[mealIndex];
-        //     : Row(
-        //   crossAxisAlignment: CrossAxisAlignment.start,
-        //   children: [
-        //     Column(
-        //       crossAxisAlignment: CrossAxisAlignment.start,
-        //       children: [
-        //         Padding(
-        //           padding: const EdgeInsets.only(left: 10, top: 10),
-        //           child: Text(
-        //             meal.
-        //             style: TextStyle(fontSize: 20),
-        //           ),
-        //         ),
-        //         Padding(
-        //           padding: const EdgeInsets.only(left: 10, top: 2),
-        //           child: Text(
-        //             restaurant.field,
-        //             style: TextStyle(fontSize: 20),
-        //           ),
-        //         ),
-        //         Padding(
-        //           padding: const EdgeInsets.only(left: 10, top: 10),
-        //           child: Text(
-        //             restaurant.restaurantName,
-        //             style: TextStyle(fontSize: 20),
-        //           ),
-        //         ),
-        //       ],
-        //     )
-        //   ],
-        // );
-        //     }
-        // );
-//       });
-// }
-
-
-// class RestaurantDetailsPage extends StatelessWidget {
-//   final RestaurantModel restaurant;
-
-//   RestaurantDetailsPage({required this.restaurant});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(restaurant.restaurantName),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Image.network(
-//               Uri.encodeFull(restaurant.restaurant_img_link.trim()),
-//               width: double.infinity,
-//               height: 200,
-//               fit: BoxFit.cover,
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(16.0),
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Text(
-//                     restaurant.restaurantName,
-//                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-//                   ),
-//                   Text(
-//                     restaurant.field,
-//                     style: TextStyle(fontSize: 18),
-//                   ),
-//                   Row(
-//                     children: [
-//                       Icon(
-//                         Icons.star,
-//                         color: Colors.yellow,
-//                       ),
-//                       Text(restaurant.rating.toString()),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceAround,
-//               children: [
-//                 Text("Menu"),                
-//                 Text("Reservations"),
-//                 Text("Reviews")
-//               ],
-//             )
-//             // Add the links for Menu, Reservation, and Reviews here
-//           ],
-//         ),
-//        )
-//       );
-//   }
-//   }
-
-//       //-------------------------------------------------------------------------------------------------------------------------------------------------------------------->
-//       // bottomNavigationBar: BottomNavigationBar(
-//       //   items: const [
-//       //     BottomNavigationBarItem(
-//       //       icon: Icon(Icons.restaurant_menu),
-//       //       label: 'Menu',
-//       //     ),
-//       //     BottomNavigationBarItem(
-//       //       icon: Icon(Icons.calendar_today),
-//       //       label: 'Reservation',
-//       //     ),
-//       //     BottomNavigationBarItem(
-//       //       icon: Icon(Icons.rate_review),
-//       //       label: 'Reviews',
-//       //     ),
-//       //   ],
-//       //   onTap: (index) {
-//       //     // Handle navigation to different sections
-//       //     // You can use a PageView or other methods to switch between sections
-//       //   },
-//       // ),
-//     );
-//    }
-//  }
